0eb6233b34b8d704e57501e3dc4c9f4202f058be
tym_lab <1060074657@qq.com>
2020-08-28 12:40:49 +0800

    initial springboot project

---
+++ online_shoping/src/main/java/com/yimin/online_shoping/OnlineShopingApplication.java
@@ -0,0 +1,13 @@
+package com.yimin.online_shoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopingApplication.class, args);
+    }
+
+}
