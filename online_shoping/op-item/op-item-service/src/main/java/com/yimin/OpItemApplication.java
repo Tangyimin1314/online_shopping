650e25817a853ccc570310504087be5d77a7b2ba
tym_lab <1060074657@qq.com>
2020-08-28 16:50:17 +0800

    init

---
+++ online_shoping/op-item/op-item-service/src/main/java/com/yimin/OpItemApplication.java
@@ -0,0 +1,13 @@
+package com.yimin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OpItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpItemApplication.class, args);
+    }
+}
---
+++ online_shoping/op-registry/src/main/java/com/yimin/OpRegistry.java
@@ -0,0 +1,13 @@
+package com.yimin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class OpRegistry {
+    public static void main(String[] args) {
+        SpringApplication.run(OpRegistry.class, args);
+    }
+}
